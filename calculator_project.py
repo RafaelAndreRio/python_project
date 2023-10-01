51ec8caca105d1b22cab9f58d0d9954e7ecebeb5
Rafael Rio <rafael_andre_rio@hotmail.com>
2023-10-01 23:52:52 +0100

    calculadora c/ expressao regular

--- calculator_project.py
+++ calculator_project.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 #Fuctions
 def clear():
@@ -7,19 +8,38 @@ def clear():
     elif os.name == 'poxis':
       os.system('clear')
 
-
-#conta=input("introduza um conta de 2 ou mais digitos com 1 ou varias operações aritmeticas\n");
-conta='2+5-8*4'
-conta_list=list(conta);
-nova_lista=[]
-
-for indice, digito in enumerate(conta_list):
-    if digito == '*' or digito == '/':
-        nova_lista.append(conta_list[indice])
-    print(f'indice {indice}, digito {digito}')
-
-print(conta_list)
-print(nova_lista)
+conta='0';
+conta_inicial=[]
+conta_list=''
+padrao = r'(\d+|[-+*/])' #expressão regular todos os inteiros e operadores escolhidos
 
 
-# * / + -
+conta=input("introduza um conta de 2 ou mais digitos com 1 ou varias operações aritmeticas\n");
+conta_inicial=conta
+conta_list = re.findall(padrao, conta) #aplicar a expressao regular
+
+
+
+while len(conta_list)>1:  
+
+  for indice, digito in enumerate(conta_list):
+    if digito == '*': 
+        conta_list.pop(indice)
+        conta_list.insert( indice-1, int(conta_list.pop(indice-1)) * int(conta_list.pop(indice-1)) )
+
+    elif digito == '/': 
+       conta_list.pop(indice)
+       conta_list.insert( indice-1, int(conta_list.pop(indice-1)) / int(conta_list.pop(indice-1)) )
+
+  for indice, digito in enumerate(conta_list):
+      if digito == '+': 
+        conta_list.pop(indice)
+        conta_list.insert( indice-1, (int(conta_list.pop(indice-1) ) + int(conta_list.pop(indice-1) ) ) )
+
+      elif digito == '-': 
+        conta_list.pop(indice)
+        conta_list.insert( indice-1, int(conta_list.pop(indice-1)) - int(conta_list.pop(indice-1)) )
+
+print("Sua conta inicial é:",conta_inicial)
+print("Resultado da conta é de:",conta_list[0])
+
